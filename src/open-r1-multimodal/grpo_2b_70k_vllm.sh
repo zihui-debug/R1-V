c48a3c5be85a2ace46456af44230c61c0df322a2
Lei Li <tobiaslee@foxmail.com>
2025-02-11 08:41:42 +0000

    update example script

--- src/open-r1-multimodal/grpo_2b_70k_vllm.sh
+++ src/open-r1-multimodal/grpo_2b_70k_vllm.sh
@@ -1,10 +1,10 @@
 export DEBUG_MODE="true"
-export LOG_PATH="./debug_log_2b_vllm.txt"
+export LOG_PATH="./debug_log_2b_rerun.txt"
 QWEN_PATH="/home/lilei/Qwen2-VL-2B-Instruct"
 HF_DATASET="/home/lilei/Clevr_CoGenT_TrainA_70K" 
 OUTPUT_DIR="/home/lilei/R1-V/checkpoints/Qwen2-VL-2B-GRPO-R1-70K" 
 
-CUDA_VISIBLE_DEVICES=0,1 torchrun --nproc_per_node="1" \
+CUDA_VISIBLE_DEVICES="0,1,2,3,4" torchrun --nproc_per_node="4" \
     --nnodes="1" \
     --node_rank="0" \
     --master_addr="127.0.0.1" \
@@ -13,18 +13,19 @@ CUDA_VISIBLE_DEVICES=0,1 torchrun --nproc_per_node="1" \
     --output_dir $OUTPUT_DIR \
     --model_name_or_path $QWEN_PATH \
     --dataset_name $HF_DATASET \
-    --max_prompt_length 256 \
-    --max_completion_length 512 \
-    --per_device_train_batch_size 4 \
-    --gradient_accumulation_steps 2 \
+    --max_prompt_length 512 \
+    --max_completion_length 1024 \
+    --temperature 1.0 \
+    --num_generations 4 \
+    --per_device_train_batch_size 1 \
+    --gradient_accumulation_steps 4 \
     --logging_steps 1 \
-    --num_generations  2 \
     --bf16  \
     --report_to wandb \
-    --gradient_checkpointing false \
+    --gradient_checkpointing true \
     --attn_implementation flash_attention_2 \
-    --max_pixels 160000 \
+    --max_pixels 400000 \
     --max_steps 13125 \
-    --run_name Qwen2-VL-2B-GRPO-R1-70K-8GPUs-nG8-maxNew4K-3epoch \
+    --run_name Qwen2-VL-2B-GRPO-R1-70K-5GPUs-nG4-maxNew1K-3epoch \
     --save_steps 1000 \
     --save_only_model true
